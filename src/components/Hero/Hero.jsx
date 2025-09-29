96bd68f4e43d01dad5e88e5c7be06a110a32bb72
Abhishek09yadav <monuy8830@gmail.com>
2025-09-30 00:00:11 +0530

    refactor: clean up Hero component structure and improve semantic HTML

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,10 +3,10 @@
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { ArrowDown } from "lucide-react";
-import { motion, time } from "motion/react";
-// import styles from "./Hero.module.css";
+import { motion } from "motion/react";
+
 const BACKGROUND_IMAGE_URL =
-  "https://images.unsplash.com/photo-1504805572947-34fad45aed93?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  "https://images.unsplash.com/photo-1504805572947-34fad45aed93?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3";
 
 const Hero = () => {
   const router = useRouter();
@@ -21,7 +21,7 @@ const Hero = () => {
     "Deploying full-stack apps with cloud platforms",
     "Passionate about modern web development",
   ];
-  // "Managing data effectively with MongoDB",
+
   const [phraseIndex, setPhraseIndex] = useState(0);
   const [text, setText] = useState("");
   const [charIndex, setCharIndex] = useState(0);
@@ -32,7 +32,6 @@ const Hero = () => {
       const typing = setInterval(() => {
         setText((prev) => prev + currPhrase[charIndex]);
         setCharIndex((prev) => prev + 1);
-
       }, 100);
       return () => clearInterval(typing);
     } else {
@@ -44,80 +43,72 @@ const Hero = () => {
       return () => clearTimeout(timeout);
     }
   }, [charIndex, phraseIndex]);
+
   return (
-    <>
-      {" "}
-      <div className="relative w-full h-dvh overflow-hidden">
-        {/* Background Image */}
-        <div
-          className="absolute inset-0 bg-cover bg-center"
-          style={{ backgroundImage: `url(${BACKGROUND_IMAGE_URL})` }}
-        />
-        <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/60 to-black/40" />
+    <div className="relative w-full h-dvh overflow-hidden">
+      {/* Background */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${BACKGROUND_IMAGE_URL})` }}
+      />
+      <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/60 to-black/40" />
 
-        {/* Content */}
-        <div className="relative z-10 flex flex-col justify-center items-center min-h-screen text-white px-4 text-center">
-          <header className="space-y-4 mb-6">
-            <motion.div className="flex mb-4 items-center justify-center gap-2 ">
-              <motion.h1
-                className="font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 text-transparent  bg-clip-text"
-                initial={{ backgroundPosition: "0% 50%" }}
-                animate={{ backgroundPosition: "50% 100%" }}
-                transition={{
-                  duration: 1,
-                  repeat: Infinity,
-                  repeatType: "reverse",
-                  ease: "easeInOut",
-                }}
-                style={{
-                  backgroundSize: "200% 200%",
-                }}
-              >
-                Hey There
-              </motion.h1>
-              <motion.h1
-                className="font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl"
-                animate={{ rotate: [0, 20, -10, 20, -10, 0] }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatDelay: 1.5,
-                }}
-              >
-                👋
-              </motion.h1>
-            </motion.div>
-            <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-5xl font-semibold mt-2 drop-shadow-lg">
-              Abhishek Here
-            </h2>
-            <p
-            // className={`text-base sm:text-lg md:text-2xl text-gray-300 typing-text ${styles.typingText}`}
+      {/* Content */}
+      <div className="relative z-10 flex flex-col justify-center items-center h-full text-white px-4 text-center">
+        <header className="space-y-4 mb-6">
+          <motion.div className="flex mb-4 items-center justify-center gap-2">
+            <motion.h2
+              className="font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 text-transparent bg-clip-text"
+              initial={{ backgroundPosition: "0% 50%" }}
+              animate={{ backgroundPosition: "50% 100%" }}
+              transition={{
+                duration: 1,
+                repeat: Infinity,
+                repeatType: "reverse",
+                ease: "easeInOut",
+              }}
+              style={{ backgroundSize: "200% 200%" }}
             >
-              {text}
-              {/* <span className={`${styles.blinkingCursor}`}>|</span> */}
-            </p>
-          </header>
-
-          <main className="max-w-2xl animate-fade-up delay-500">
-            <p className="text-sm sm:text-base md:text-xl leading-relaxed text-gray-200">
-              I build scalable, high-performance web applications using MongoDB,
-              Express.js, React, and Node.js. Let’s bring your ideas to life
-              with clean code and intuitive design.
-            </p>
-          </main>
-
-          <footer className="mt-10 animate-fade-up delay-700">
-            <button
-              onClick={() => router.push("/projects")}
-              className="inline-flex cursor-pointer items-center gap-2 px-6 py-3 text-lg font-medium text-white bg-gradient-to-r from-blue-600 to-indigo-700 rounded-full shadow-lg hover:scale-105 hover:shadow-xl transition-transform duration-300"
+              Hey There
+            </motion.h2>
+            <motion.span
+              className="font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl"
+              animate={{ rotate: [0, 20, -10, 20, -10, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatDelay: 1.5,
+              }}
             >
-              View My Work
-              <ArrowDown className="w-5 h-5" />
-            </button>
-          </footer>
-        </div>
+              👋
+            </motion.span>
+          </motion.div>
+
+          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-5xl font-semibold mt-2 drop-shadow-lg">
+            Abhishek Here
+          </h1>
+          <p>{text}</p>
+        </header>
+
+        <main className="max-w-2xl animate-fade-up delay-500">
+          <p className="text-sm sm:text-base md:text-xl leading-relaxed text-gray-200">
+            I build scalable, high-performance web applications using MongoDB,
+            Express.js, React, and Node.js. Let’s bring your ideas to life with
+            clean code and intuitive design.
+          </p>
+        </main>
+
+        <footer className="mt-10 animate-fade-up delay-700">
+          <button
+            onClick={() => router.push("/projects")}
+            className="inline-flex cursor-pointer items-center gap-2 px-6 py-3 text-lg font-medium text-white bg-gradient-to-r from-blue-600 to-indigo-700 rounded-full shadow-lg hover:scale-105 hover:shadow-xl transition-transform duration-300"
+          >
+            View My Work
+            <ArrowDown className="w-5 h-5" />
+          </button>
+        </footer>
       </div>
-    </>
+    </div>
   );
 };
 
